74f46a5463ef094558e282f19e61293fba17aceb
Manuel Parra <manuelparra@live.com.ar>
2019-10-16 11:19:31 -0400

    Primera app con Angular

---
+++ src/app/saludar/saludar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-saludar',
+  templateUrl: './saludar.component.html',
+  styleUrls: ['./saludar.component.scss']
+})
+export class SaludarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
